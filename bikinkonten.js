63b83fcaae9529617c512e2af33fe036f0a457ba
Wpreneurs <114202814+Wpreneurs@users.noreply.github.com>
2026-03-10 16:23:37 +0700

    Update and rename pwasimpeltoko.js to bikinkonten.js

--- bikinkonten.js
+++ bikinkonten.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgopL24auHdv-V4OLM6wAWO1iFEeSmxzAGH1Dh_s3fmo6G6Szf3yINXirv-okTZStamKvpwuZE0vnWvpT9fc6nO4pVCbgVT3hLFMwBk46ZjxB65zuMc5AC3aycM65wsuyuouGw8QxEHakwUo10deyV6UwjxhfQ6n0BuHKtWHcTxphrheDxwjqQqBdlODA4d/s16000/AI%20SCRIPT%20GENERATOR%20512x512.jpg"
       ]);
     })
   );
@@ -19,3 +19,4 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
